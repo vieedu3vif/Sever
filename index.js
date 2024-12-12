9804e7e8cdb141b77a86c70f6c0c14bd2093cc24
unknown <ducminhphoco@gmail.com>
2024-12-12 23:13:10 +0700

    change

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ async function checkAndNotify() {
     if (data) {
       console.log(`Telemetry data for device ${deviceId}:`, data);
 
-      if (data.temperature && data.temperature[0]?.value > 37.2) {
+      if (data.temperature && data.temperature[0]?.value > 37) {
         sendNotification(
           "Bệnh nhân bất thường",
           `Nhiệt độ: ${data.temperature[0].value}°C, vượt ngưỡng!`
